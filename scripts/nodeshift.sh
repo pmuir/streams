5299b57f7337a545d4411cfc18a3568cade6b2f8
Doug Donahue <ddonahue@redhat.com>
2020-10-07 16:28:35 -0400

    default to min scale 1

--- scripts/nodeshift.sh
+++ scripts/nodeshift.sh
@@ -29,7 +29,7 @@ deploy() {
   ${NODESHIFT_CMD} --knative=true --namespace.name=${NAMESPACE}
   dd-oc label ksvc/${KSVC_NAME} app.kubernetes.io/part-of=${APP_NAME} --overwrite=true
 
-  kn service update ${KSVC_NAME} --min-scale 0
+  kn service update ${KSVC_NAME} --min-scale 1
 
   local _latest_rev=$(oc --namespace ${NAMESPACE} get ksvc/${KSVC_NAME} -o=jsonpath='{.status.latestCreatedRevisionName}')
   dd-oc label rev/${_latest_rev} app.openshift.io/runtime=nodejs --overwrite=true
