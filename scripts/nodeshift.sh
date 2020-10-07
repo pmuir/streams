af5874f170183aff6ed5397169a7a68f1096110e
Doug Donahue <ddonahue@redhat.com>
2020-10-07 16:26:47 -0400

    Updated deployment script

--- scripts/nodeshift.sh
+++ scripts/nodeshift.sh
@@ -4,14 +4,13 @@ DIR=$(dirname "$0")
 source ${DIR}/common/logger.sh
 source ${DIR}/common/util.sh
 
-NODESHIFT_CMD=`pwd`/node_modules/.bin/nodeshift
-
 usage() {
-  log-info "Usage: $(basename $0) [deploy|undeploy] <app_name>"
+  log-info "Usage: $(basename $0) [deploy|undeploy] <service_name>"
   exit 1
 }
 
-APP_NAME=${APP_NAME:-3scale}
+NODESHIFT_CMD=`pwd`/node_modules/.bin/nodeshift
+APP_NAME=${APP_NAME:-ums-poc}
 
 NAMESPACE=${NAMESPACE}
 if [ -z "${NAMESPACE}" ]; then
@@ -21,7 +20,7 @@ fi
 
 KSVC_NAME=$2
 if [ -z "${KSVC_NAME}" ]; then
-  log-info "You must specify and app name!"
+  log-info "You must specify and service name!"
   usage
 fi
 
@@ -29,8 +28,8 @@ deploy() {
   log-info "nodeshift --knative=true --namespace.name=${NAMESPACE}"
   ${NODESHIFT_CMD} --knative=true --namespace.name=${NAMESPACE}
   dd-oc label ksvc/${KSVC_NAME} app.kubernetes.io/part-of=${APP_NAME} --overwrite=true
-  dd-oc annotate --overwrite ksvc/${KSVC_NAME} app.openshift.io/connects-to=streams-api
-  dd-oc annotate --overwrite ksvc/${KSVC_NAME} autoscaling.knative.dev/minScale=1
+
+  kn service update ${KSVC_NAME} --min-scale 0
 
   local _latest_rev=$(oc --namespace ${NAMESPACE} get ksvc/${KSVC_NAME} -o=jsonpath='{.status.latestCreatedRevisionName}')
   dd-oc label rev/${_latest_rev} app.openshift.io/runtime=nodejs --overwrite=true
